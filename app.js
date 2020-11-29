07faef222b93157dd3685534aac4f59a60b1ca1a
Simron Shrestha <simron6917@gmail.com>
2020-11-29 11:33:36 +0545

    recent one..

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const express= require("express");
 const bodyParser= require("body-parser");
 const ejs= require("ejs");
 const mongoose= require("mongoose");
-const encrypt= require("mongoose-encryption");
+
+//md5 ko satta bcrypt use garya with saltrounds.
+const bcrypt = require("bcrypt");
+const saltRounds= 10;
 
 const app= express();
-
-
-
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -24,7 +24,6 @@ const userSchema= new mongoose.Schema({
 
 //encryption use garya...pw matra encrypt gareko
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"] });
 
 const User= new mongoose.model("User", userSchema);
 
@@ -39,36 +38,42 @@ app.get("/login", function(req,res){
 app.post("/login", function(req,res){
     const username=req.body.username;
     const password=req.body.password;
-    
+     
     User.findOne({email:username}, function(err, foundUser){
         if(err){
 console.log(err);
         } else{
             if(foundUser){
-                if(foundUser.password===password){
+                bcrypt.compare(password, foundUser.password, function(err, result){
+                   if(result === true){
                     res.render("secrets");
                 }
-            }
+            });
         }
+    }
     });
 });
 app.get("/register", function(req,res){
     res.render("register");
 });
 app.post("/register", function(req,res){
-    const newUser= new User({
-        email: req.body.username,
-        password:req.body.password
+    
+    bcrypt.hash(req.body.function, saltRounds, function(err, hash){
+        const newUser= new User({
+            email: req.body.username,
+            password: hash
+        });
+    newUser.save(function(err){
+        if(!err){
+            res.render("secrets");
+        } else {
+            console.log("error found");
+        }
     });
-newUser.save(function(err){
-    if(!err){
-        res.render("secrets");
-    } else {
-        console.log("error found");
+    });
+   
+});
 
-    }
-});
-});
 app.listen(3000, function(){
     console.log("Server started on port 3000");
    });
